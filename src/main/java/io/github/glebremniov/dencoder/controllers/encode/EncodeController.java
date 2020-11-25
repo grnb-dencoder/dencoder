cb129511415ae3385edfdbab2a2d74eb5fe1d49b
glebremiov <remniov.work@gmail.com>
2020-11-25 16:53:47 +0300

    Minor fix

--- src/main/java/io/github/glebremniov/dencoder/controllers/encode/EncodeController.java
+++ src/main/java/io/github/glebremniov/dencoder/controllers/encode/EncodeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/encode/")
+@RequestMapping(path = "/api/encode")
 public class EncodeController {
 
     private final EncodeService encodeService;
